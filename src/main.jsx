e0657d3b66dbda52ac73033aaf2e29c073ea5ab2
Abdullah Al Asif <74261642+aaaasif@users.noreply.github.com>
2022-02-21 10:01:04 +0600

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+// eslint-disable-next-line
+import "swiper/css/bundle";
+import "./styles.css";
+
+import App from "./App.jsx";
+
+ReactDOM.render(<App />, document.getElementById("app"));
+    
